e64bc65d2181864dd525c0dc64ad0069d86ef590
haxjump <dafanhui@163.com>
2022-06-04 18:56:29 +0800

    add pprof in benchmark cases

--- benches/smt_benchmark.rs
+++ benches/smt_benchmark.rs
@@ -1,5 +1,8 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use pprof::ProfilerGuard;
 use rand::{thread_rng, Rng};
+use std::fs::File;
+use vsdb::VsMgmt;
 use xsmt::{
     blake3_hasher::Blake3Hasher, default_store::DefaultStore, tree::SparseMerkleTree,
     H256,
@@ -17,14 +20,22 @@ fn random_h256(rng: &mut impl Rng) -> H256 {
 }
 
 fn random_smt(update_count: usize, rng: &mut impl Rng) -> (SMT, Vec<H256>) {
-    let mut smt = SparseMerkleTree::default();
+    let mut smt = SMT::default();
+    smt.version_create(b"".as_slice().into()).unwrap();
     let mut keys = Vec::with_capacity(update_count);
+
+    let guard = ProfilerGuard::new(1000).unwrap();
+
     for _ in 0..update_count {
         let key = random_h256(rng);
         let value = random_h256(rng);
         smt.update(key, value).unwrap();
         keys.push(key);
     }
+
+    let file = File::create(format!("/tmp/xsmt_benchmark_{}.svg", update_count)).unwrap();
+    guard.report().build().unwrap().flamegraph(file).unwrap();
+
     (smt, keys)
 }
 
